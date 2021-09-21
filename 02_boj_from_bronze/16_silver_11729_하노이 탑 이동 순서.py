15b0ad5369e9c435e02797c86ee6e54d58347523
Jaeseong Cho <tingstyle1@gmail.com>
2021-09-21 11:20:27 +0900

    하노이 재풀이 및  최소공배수, date관련 문제 풀기

--- 02_boj_from_bronze/16_silver_11729_하노이 탑 이동 순서.py
+++ 02_boj_from_bronze/16_silver_11729_하노이 탑 이동 순서.py
@@ -1,15 +1,50 @@
 # 느낀점 : 
+# - n by n-1뿐만 아니라... 다른요소들까지 부분문제에서 달라지면(start, end기둥) 인자로 받아줘야한다.
+# - 각 부분문에서 return으로 끝내는 대신 print 이동경로를 출력함. 
+# - 3수 고정 -> 3수의 합 고정 -> 2개 알면 1개 추론 가능.
+# - n-1 / print / n-1이며, 부분문제에서 알아서 hanoi(n-1)도 출력될 것이다.. 
+# **- 부분문제 정복 n-1에서도 이미 내부 구조가 있기 때문에 ---> n상황에서만 print를 하면, 알아서 또 재귀적으로 print될 것임.**
+
 ################ Input From input.txt ################
 import sys
 from pprint import pprint
 sys.stdin = open("./input.txt", "rt")
 ######################################################
-# https://nanarin.tistory.com/203 -> 3번째 풀이 참고
-# 느낀점
-# - 문자열은 재귀시 바로 append 등 n//3 으로 n모양을 만들도록 append extend 등을 할 수가 없다.
-# - 각 줄바꿈 단위별로 잘라서 1차원을 만든다. -> 나중에 join시 \n로 join하면 line다 다 선다.
-# **- 부분문제를 변수로 받아서 처리하기도 한다.**
-# ** 문자열의 재귀는 '\n'.join으로 다 세울거니까 --> 1차원 list로 요소들을 다 만들면된다.**
 
-N= int(input()) # 그 당시(제일 큰)의 빈 행렬을 만들기 위해 받아놓고 바로 처리
+N= int(input()) 
 
+# hanoi (n)번째는,, 1->3으로 가는데
+# hanoi(n-1)을 1->2번기둥으로 -> n번째원판 1->3으로 -> hanoi(n-1)를 2->3으로 한번더 
+
+# 1) 맨 밑 원판 제외 start -> other
+# 2) 맨 밑 원판 start->end 
+# 3) 제외원판 other -> end  
+# 4) 1->3으로 갈때는 2 ,  2->3으로 갈때는1, start -> end로 갈 때는 6-(start+end)
+#  -> 3수가 일정 = 3수의 합이 일정 -> 나머지 1개 추론가능
+
+count_list = []
+def hanoi(n, start, end):
+    
+    if n == 1:
+        # start, end는 뭐가올지.. 점화식에서 결정되니.. 그것을 출렴난 해주자. 그리고 종료까지..?
+        #print(start, end)
+        count_list.append((start, end))
+        return
+
+    # n-1을 처음에 end가 아닌  다른기둥에 옮겨야한다.
+    other = 6-(start+end)
+    hanoi(n-1, start, other) 
+    #print(start, end)
+    count_list.append((start, end))
+    hanoi(n-1, other, end) 
+
+
+hanoi(N, 1, 3)
+print(len(count_list))
+for data in count_list:
+    print(*data)
+
+
+
+
+    
--- 99_기타구현_코드만/02_gcd_and_lcm.py
+++ 99_기타구현_코드만/02_gcd_and_lcm.py
@@ -51,3 +51,96 @@ def gcd2(n, m):
     return gcd
 
 print(f'❤3 gcd2({x},{y}) with factorize2, commFactors: ', gcd2(x, y))
+
+
+# 4. lcm : 최소공배수 풀기1 - 두 수의 곱과 gcd만 알면 (gcd*a*b인 lcm을) 바로 구한다.
+# A = GCD * a  / B = GCD * b  
+# -> A*B = GCD^2 * a * b -> 두 수의 곱에서 gcd 한번만 나눠주면 lcm이다.
+# lcm = GCD * a * b  =========   A*B/GCD
+# -> gcd는 math모듈에서 쉽게 추출할 수 있다.
+import math
+
+def lcm(m,n):
+    return (m*n) // math.gcd(m,n)
+
+print(f'❤4 lcm({x},{y}) with math.gcd: ', lcm(x, y))
+
+
+# 5. 1부터 10까지 lcm은?
+# - 공간최적화처럼 업데이트한다. 돌면서 어딘지는 모르지만 min(,) max(,)를 찾아냈던 것처럼..
+# - 직전까지의 lcm.. for문안에서는 현재의 lcm으로 업데이트시켜주면서 돌기..
+# - curr(현재의) <- func( curr(직전까지의), new)
+# - 어떤 값이 있고, 추가된 숫자로 계싼을 한뒤, 해당 변수에 update시키는 누적계산
+# -- 변수1 for  변수1 = 계산(변수1, 새로운값)  -> 변수1을 최신으로 update
+# -- 변수이자 결과값을 담는 곳-> 현재로 업데이트
+
+curr_lcm = 1
+for i in range(2, 10+1):
+    curr_lcm = lcm(curr_lcm, i)
+print(curr_lcm)
+
+def lcm_from_to(start, end):
+    curr = start 
+    for num in range(start+1, end+1):
+        curr = lcm(curr, num)
+
+    return curr 
+
+print(f'5 lcm_from_to(1,10) with math.gcd: ', lcm_from_to(1,10))
+
+
+# 6. lcm2 with lcmFactors : 두수의 곱과 gcd를 이용한 쉽게 푸는 방법이 아니라
+#    소인수분해후, 인수를 뽑아내서 최소공배수를 만들어보자.
+# - 최대_공약수 : 소인수분해후 공통이면 다 뽑아내기(작은것을 버리고 큰쪽에서 기다림)
+# - 최소_공배수 : 소인수분해후 lcm에 해당하는 GCD + a + b 다 뽑아내기
+#   -> 작은 것도 안버리고 챙긴다. a or b 에 해당할 것이기 때문.
+#  2-2 2-2 [2]-3 3-3 4-[3]  -> 2와 3을 버려야할 것 같지만, GCD외에 전부 a, b에 해당하므로 챙겨야한다.
+
+def lcmFactors(factorized_n,factorized_m):
+    factors = []
+    i, j = 0, 0
+    # 조건이 2개가 and로 걸려있으면 -> 어느것이 끝나서 종료되었는지 나중에 확인
+    while i < len(factorized_n) and j < len(factorized_m):
+        if factorized_n[i] == factorized_m[j]:
+            factors.append(factorized_n[i])
+            i+=1
+            j+=1
+        elif factorized_n[i] < factorized_m[j]:
+            factors.append(factorized_n[i])
+            i+=1
+        else:
+            factors.append(factorized_m[j])
+            j+=1
+
+    # 아직 안끝났다면.. 챙겨라.
+    if i < len(factorized_n):
+        factors.extend(factorized_n[i:])
+    if j < len(factorized_m):
+        factors.extend(factorized_m[j:])
+
+    return factors
+
+print(f'6-1 lcmFactors([1,2,3],[2,3,5]) with factorize2 아직 lcm되기엔 누접곱이 남음.: ', lcmFactors([1,2,3],[2,3,5]))
+
+# 6-2. lcm2 with factorize2 + lcmFactors 
+def lcm2(n,m):
+    f1 = factorize2(n)
+    f2 = factorize2(m)
+    factors = lcmFactors(f1, f2)
+    # [ GCD 1개, a들, b들] -> 누적곱해야함.
+
+    curr = factors[0]
+    for i in factors[1:]:
+        curr*=i
+    return curr
+
+
+print(f'6-2 lcm2(10,12) with lcmFactors : ', lcm2(10, 12))
+
+
+
+
+
+
+
+
