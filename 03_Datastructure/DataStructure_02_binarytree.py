342717ef9660f61d3c598eee48696a4c0982f48a
Jaeseong Cho <tingstyle1@gmail.com>
2021-10-23 21:40:38 +0900

    heap을 제외한 자료구조 구현 끝

--- 03_Datastructure/DataStructure_02_binarytree.py
+++ 03_Datastructure/DataStructure_02_binarytree.py
@@ -154,3 +154,203 @@ class BinSearchTree:
 
 
 
+#### 이진탐색 트리 remove까지 풀구현(insert만 넣은 부분 위에 살아있음)
+class Node:
+
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, key, data):
+        if key < self.key:
+            if self.left:
+                return self.left.insert(key, data)
+            else:
+                self.left = Node(key, data)
+        elif key > self.key:
+            if self.right:
+                return self.right.insert(key, data)
+            else:
+                self.right = Node(key, data) 
+        else:
+            raise KeyError('Key %s already exists.' % key)
+
+    def inorder(self):
+        traversal = []
+        if self.left:
+            traversal += self.left.inorder()
+        traversal.append(self)
+        if self.right:
+            traversal += self.right.inorder()
+        return traversal
+    
+    # NEW1. lookup
+    def lookup(self, key, parent=None):
+        if key < self.key:
+            if self.left:
+                return self.left.lookup(key, self) # self = left subtree의 부모 Node
+            else:
+                return None, None
+        elif key > self.key:
+            if self.right:
+                return self.right.lookup(key, self) # self = left subtree의 부모 Node
+            else:
+                return None, None
+        else:
+            return self, parent
+        
+    # NEW2. countChildren : 삭제시, 자식의 갯수에 따라 경우가 3가지로 달라짐.
+    def countChildren(self):
+        count = 0
+        if self.left:
+            count+=1
+        if self.right:
+            count+=1
+        return count
+            
+    # insert처럼, remove도 node에서는 없고 Tree에만 있다.
+    # countChildren는 node에만 있다.
+    # inorder, lookup은 node, Tree 둘다 있다.
+
+class BinSearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key, data):
+        if self.root:
+            self.root.insert(key, data)
+        else:
+            self.root = Node(key, data)
+
+
+    def inorder(self):
+        if self.root:
+            return self.root.inorder()
+        else:
+            return []
+        
+    # NEW3. 
+    def lookup(self,key):
+        if self.root:
+            return self.root.lookup(key)
+        else:
+            return None, None
+        
+    # NEW4. remove는 어려워서 그런지, 기본적인 주석을 참고해서 만든다.
+    def remove(self, key):
+        # 1. 일단 Tree lookup -> node lookup의 node, 부모node를 return 받는다.
+        node, parent = self.lookup(key)
+        # 2. 찾는 node가 있을때만 삭제가 가능하다. 없으면 return False
+        if node:
+            # 3. 찾은 node에서, node.countChildren로 그놈의 자식갯수를 파악하고 , 그 경우의수를 나눈다.
+            nChildren = node.countChildren()
+            # The simplest case of no children
+            # 4. 자식이 없는 경우
+            if nChildren == 0:
+                # 만약 parent 가 있으면
+                # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # parent.left 또는 parent.right 를 None 으로 하여
+                # leaf node 였던 자식을 트리에서 끊어내어 없앱니다.
+                # 4-1. 삭제하는 노드가 루트노드인지 확인한다.
+                if parent:
+                    # 4-2. 루트노드가 아니면 == 부모가 있으면, 부모의 왼쪽인지/ 오른쪽인지 확인하여, 
+                    #      삭제된다 = 부모의 해당쪽 tree가 사라진다.(자식이 없으므로 딸려올라오는 것도 없다.)
+                    #      원래 자식이 있으면, 자식를 챙겨놓고, 그 자식을 parent의 link에 대입해줘야함.
+                    if node == parent.left:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                        
+                # 만약 parent 가 없으면 (node 는 root 인 경우)
+                # self.root 를 None 으로 하여 빈 트리로 만듭니다.
+                # 4-3. 삭제하려는놈이 부모가 없다 == root node이다.
+                else:
+                    self.root = None
+                    #4-4. tree를 비워주는 self.root = None을 시행해준다.
+                    
+            # When the node has only one child
+            elif nChildren == 1:
+                # 하나 있는 자식이 왼쪽인지 오른쪽인지를 판단하여
+                # 그 자식을 어떤 변수가 가리키도록 합니다.
+                #5. 자식이 1개인 경우, 자식이 그대로, 삭제된node의 자리로 가서, 삭제된node의 parent의 해당link에 딸려올라가기 때문에, 
+                #   자식tree를  childNonde라는 변수에 미리 챙겨둔다.
+                if node.left:
+                    childNode = node.left
+                else:
+                    childNode = node.right
+                    
+                # 만약 parent 가 있으면
+                # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
+                # 위에서 가리킨 자식을 대신 node 의 자리에 넣습니다.
+#               # 6. 자식이 없는 경우 부모를 먼저 챙겼지만, 자식있는 경우 자식챙기고 -> 부모의 어느쪽 제거인지 확인 -> None대신 자식을 넣어주면 된다.
+                if parent:
+                    if node == parent.left:
+                        parent.left = childNode
+                    else:
+                        parent.right = childNode
+                # 만약 parent 가 없으면 (node 는 root 인 경우)
+                # self.root 에 위에서 가리킨 자식을 대신 넣습니다.
+                #7. 부모가 없는 경우 == root node인 경우, 자식을 root에 줌.
+                else:
+                    self.root = childNode
+            # When the node has both left and right children
+            else:
+                #8. 자식이 양쪽으로 다있는 경우, 특이하다
+                # 1) 삭제대상보다 key가 한칸 더 큰 노드를 바로 오른쪽 subtree로 이동 -> 제일 작은 값을 찾기 위해 왼쪽subtree만 타기  ->  찾는다.
+                # 2) 그놈을 S, 그놈의 부모를 P라고 챙겨놓고,  S --> 삭제자리에 배치한다. 이제 기존 S만 삭제하면 된다.
+                # 3) P의 자식을 삭제하는 것이므로 P의 자식링크에 None을 넣으면 된다. 하지만, 문제가 p == 삭제대상인 경우도 있다.
+                #   X = P
+                #  / \
+                # a   S (X에서 한칸 오른쪽 갔는데, 더이상 갈 곳이 없다.)
+                # 자식이 2개인 X지만,, X == P가 되는 상황이 있음.
+                #   S = P 
+                #  / \
+                # a   S  :  P의 자식링크에 None을 넣는 것을 이 경우에만 p.right 에다가 None을 넣고, 그 외에서는 P.left에다가 None을 넣어 기존 S를 삭제한다.
+                
+                #8-1, S와 P를 챙긴다. : key 한칸 증가한 것 = 오른쪽으로 한칸 간 것 = S와 , 그것의 부모는 /\ 가기 전 node가 되므로 S와 P를 챙긴다.
+                parent = node
+                successor = node.right
+                # parent 는 node 를 가리키고 있고,
+                # successor 는 node 의 오른쪽 자식을 가리키고 있으므로
+                # successor 로부터 왼쪽 자식의 링크를 반복하여 따라감으로써
+                # 순환문이 종료할 때 successor 는 바로 다음 키를 가진 노드를,
+                # 그리고 parent 는 그 노드의 부모 노드를 가리키도록 찾아냅니다.
+                #8-2. 이제 S를 왼쪽으로 갈 수 있는 가야한다.
+                # - 왼쪽으로 간다. == 직전node.left를  node로 업데이트 시키는데, < 왼쪽이 존재하는 동안 계속 > while 
+                while successor.left:
+                    parent, successor = successor, successor.left
+                # 삭제하려는 노드인 node 에 successor 의 key 와 data 를 대입합니다.
+                #8-3. 갈수있는만큼 다 갔으면, S의 < key와 data값 대입함으로써 > --> < S를 삭제할 X 자리로 옮김 >을 시행한다.
+                node.key = successor.key
+                node.data = successor.data
+                # 이제, successor 가 parent 의 왼쪽 자식인지 오른쪽 자식인지를 판단하여
+                # 그에 따라 parent.left 또는 parent.right 를
+                # successor 가 가지고 있던 (없을 수도 있지만) 자식을 가리키도록 합니다.
+                # 8-4. 이제 기존S만 삭제하면 된다.
+                # - 보통은 S가 P의 left에 있으므로 p.left에 s의 자식들을 끌어올리면 되지만, 
+                # - X = P가 되는 경우도 있으니, S가 p.right가 될때만, p.right에 s의 자식들을 끌올린다.
+                if successor == parent.left:
+                    # 8-5. 이 때, successor.right는 <왼쪽의 극한이기 때문에 .left는 존재안하며> <있을 수도, 없을 수도 있으며, 있다면 반드시 right에만 있을 S의 자식>을 끌어올려 할당해주자.
+                    #   P
+                    #  /
+                    # S  or
+                    #     P
+                    #    /
+                    #   S
+                    #    \
+                    #     a
+                    parent.left = successor.right
+                else:
+                    parent.right = successor.right
+
+            return True
+
+        else:
+            return False
+
+
+def solution(x):
+    return 0
