97cffa0bd575a81056c393b941c54c1400636d56
Jaeseong Cho <tingstyle1@gmail.com>
2021-10-24 22:04:43 +0900

    bitmask를 위한 이진수연산 문제 풀이

--- 04_problems_by_class/test.py
+++ 04_problems_by_class/test.py
@@ -1,6 +1,3 @@
-# 느낀점: 
-# a % b 에서 b자리에 더 큰수가 있다면 a가 그대로 나온다.
-# 
 ################ Input From input.txt ################
 import sys
 from pprint import pprint
@@ -9,31 +6,32 @@ sys.stdin = open("./input.txt", "rt")
 ### 백준용
 ### import sys
 ### input = sys.stdin.readline
-# 단계별로 푸시는 분: https://developer-ellen.tistory.com/33 # 고민 많이 하시는 분이라 챙겨놓음.
-# 지져스 python 해설 블로그: https://peisea0830.tistory.com/category/BOJ # 해설&그림으로 미쳤음.
-# bisect를 최초로 사용한 풀이: https://zereight.tistory.com/672 # N과M 1~11 등 실력순으로 푸신 것 같은데 검색기능이 없다.
 ######################################################
-
 import sys
-input = sys.stdin.readline
+#  A & B, A | B, A ^ B, ~A, ~B
+
+a = int(input(), base=2)
+b = int(input(), base=2)
+
+# ~ 연산은 2의보수로 출력된다.
+# -> 반전의 계산을 할게 아니라, 반전출력용은 xor1111111111로 해준다.
+# n자리 111111111를 만드는 방법은?
+one_mask = 2^(10**6)-1
+# 근데.. 0001000에서 앞에 000도 반전시켜벌임.. 0이어야함..
+
+# bin()을 때리면, string이며, 0b를 제외하고, 앞쪽 0은 다 짤린 문자열
+# -> len()-1로 가장  첫번째의 인덱스(뒤에서 0부터 시작하니)
+# -> index는 필요없고 그len()만큼 1로 채워지면 될듯?
 
 
-
-while True:
-    a, b= map(int, input().split())
-    if a ==0 and b==0:
-        break
-    
-    elif b % a == 0:
-        print("factor")
-        
-    elif a % b == 0:
-        print("multiple")
-    else:
-        print("neither")
-        
+a_len = len(bin(a)[2:])
+a_mask = int("1"*100000, base=2)
+b_len = len(bin(b)[2:])
+b_mask = int("1"*100000, base=2)
 
 
-
-
-
+print(bin(a&b)[2:].rjust(10000,"0"))
+print(bin(a|b)[2:].rjust(100000,"0"))
+print(bin(a^b)[2:].rjust(100000,"0"))
+print(bin(a^a_mask)[2:].rjust(100000,"0"))
+print(bin(b^b_mask)[2:].rjust(100000,"0"))
