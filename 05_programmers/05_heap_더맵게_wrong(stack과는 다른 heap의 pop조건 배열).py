57daa1576546ab733e87384cf1285aa63cc09f85
Jaeseong Cho <tingstyle1@gmail.com>
2021-10-30 18:14:20 +0900

    프로그래머스 5번까지 공부 완료

--- 05_programmers/05_heap_더맵게_wrong(stack과는 다른 heap의 pop조건 배열).py
+++ 05_programmers/05_heap_더맵게_wrong(stack과는 다른 heap의 pop조건 배열).py
@@ -1,9 +1,28 @@
 # 느낀점:
+# * 1. 가장~ 한 것을 뽑을 때, 2개이상 연속해서 뽑아야한다면, sorting 및 sort유지 자료구조 heap을 고려하자.
+# ** -> <데이터 push/pop> 시 정렬을 유지하는 자료구조 -> heap
+# * 2. 이 문제에서 일반 list로 계산시, -> 가장 작은 것 뽑기 N, 2번째도 N-1개 다 살피기 -> 계산에서 -> 가장 작은 자리 넣기 N -> n개를 반복 -> N*N
+# ** heap은 매번 lgN으로 넣기/뽑기함. -> N개 * lgN
+# * 3. stack과 다르게  조건을 줘야한다.
+# ** 3-1) pop전에 while stack 담에 and stack[-1]의 peek보고 주는 조건을 ----> heap에서는 주지못한다. 
+# ** 3-2) while hp -> min = heappop() 해서 [-1]값을 뽑아 -> if 조건을 내부에 하나 더 주는데, 
+# ***    [while 도는 조건, 직전까지]  대신, [if while 탈출조건]으로 조건을 건다.
+# * 4. 2개 연속뽑더라도, pop전에 heap 비어있지 않은지 조건을 매번 달아줘야한다. 비어있으면 에러 종료
+# while scoville: # 4-1) [-1]을 빼올 수가 없어 while에서 추가  도는 조건으로 못줌.
+#     first = hq.heappop(scoville) # 4-2) 내부에서 직접뽑아쓰면서, 탈출조건으로 줘야함.
+#     if first>=K:
+#         break
+#     if not scoville: # 4-3) 연속에서 뽑아야한다면, pop전에 비지 않았는지 누적해서 검색해야함.
+#         return -1
+#     second = hq.heappop(scoville)
+#     hq.heappush(scoville, first + second*2)
+#     answer+=1
 ################  ################
-# - 가장 맵지 않은 두 음식을 골라야 하므로, sorting은 필수!
+
+# 해설 모음)
+# 가장 맵지 않은 두 음식을 골라야 하므로, sorting은 필수!
 # 여기서 중요한 점은 데이터를 저장하고 삭제하는 과정에서 정렬 상태를 유지해야한다는 것입니다. 그럼 섞을 때마다, 정렬을 해주면 되겠군요! 즉, 다음처럼 풀수 있겠지요.
 # 데이터 저장/삭제 시 정렬을 유지하는 자료구조를 알고 있습니다. 맞습니다. "힙" 혹은 "우선 순위 큐"
-# 아~ 연속해서 뽑아도 <내부에서는 정렬을 유지하는 자료구조>로서 가장 작은 순으로뽑아지도록 해야해서.. heap을 쓰는 구나.. 최소힙..
 
 # 최악의 경우 :수가 하나 남을 때까지 섞어야 하는 경우(n-1회)
 # if 리스트: 정렬된 리스트에 순서 맞추어 원소 삽입 O(n)
@@ -49,23 +68,7 @@ def solution(scoville, K):
 
 
 # 힙의 저장과 삭제 연산은 O(log(N))의 시간 복잡도를 가집니다. 또한, 문제에서 최악의 경우, scoville 길이 N만큼 반복하기 때문에, 위 알고리즘의 시간 복잡도는 O(N * log(N))입니다. 실제 코드를 제출하면 정확성 테스트까지 모두 통과하는 것을 확인
-import heapq as hq
 
-def solution(scoville, K):
-    hq.heapify(scoville)
-    cnt = 0    
-    while min(scoville) < K:
-        if len(scoville)<2:
-            return -1   
-        
-        low_1, low_2 = hq.heappop(scoville), hq.heappop(scoville)
-        if low_2 == 0:
-            return -1
-        
-        hq.heappush(scoville, low_1 + low_2*2)
-        cnt+=1
-        
-    return cnt
 
 # ==========효율성 미통과 코드===============
 # import heapq as hq
