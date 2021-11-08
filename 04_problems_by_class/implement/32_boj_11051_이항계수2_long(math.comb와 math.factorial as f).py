ce6c3b411b6991212df570a905bf34c70f91d5c7
Jaeseong Cho <tingstyle1@gmail.com>
2021-11-08 15:14:59 +0900

    34번까지 문제풀이 느낀점 정리

---
+++ 04_problems_by_class/implement/32_boj_11051_이항계수2_long(math.comb와 math.factorial as f).py
@@ -0,0 +1,24 @@
+# 느낀점:
+# *1. itertools combinations, permutations 말고도, -> math 모듈에 nCk를 계산하는 math.comb 와   factorial을 계산해주는 math.factorial as f 도 있다.
+################ Input From input.txt ################
+import sys
+from pprint import pprint
+sys.stdin = open("./input.txt", "rt")
+
+### 백준용
+### import sys
+### input = sys.stdin.readline
+# ######################################################
+
+# nCk = n! / k! (n-k)!     n /n-k     n-1! / k! (n-1 -k) n-1Ck
+# n
+# from math import factorial as f
+# n, k = map(int, input().split())
+# print(int(f(n)/(f(n-k)*f(k))))
+
+# import math
+# print(math.comb(*map(int,input().split())))
+import math 
+N, K = map(int, input().rstrip().split())
+
+print(math.comb(N,K) % 10007)
